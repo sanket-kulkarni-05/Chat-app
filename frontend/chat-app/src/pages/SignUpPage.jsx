ce09b5efe3d359cdae381df3b2429051790f73e5
Sanket Kulkarni <sanketkulkarni2005@gmail.com>
2025-07-21 18:37:15 +0530

    Removed unused variables and imports.

--- frontend/chat-app/src/pages/SignUpPage.jsx
+++ frontend/chat-app/src/pages/SignUpPage.jsx
@@ -1,3 +1,5 @@
+// Practise Code
+
 // import { useState } from "react";
 // import { useAuthStore } from "../store/useAuthStore";
 // import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
@@ -154,9 +156,7 @@ import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
-import AuthImagePattern from "../components/AuthImagePattern";
-import toast from "react-hot-toast"; // <-- Make sure to import toast
-import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast"; 
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
